da992410acad71692cacaead49f0eb5edab49b64
lberwa <lew.brandt@gymalf.de>
2026-06-19 12:04:51 +0200

    commit 2

--- pip/boot.py
+++ pip/boot.py
@@ -1,6 +1,12 @@
 import wiitools as w
+import json
 
-data =
+data = None
+get = []
+urls = ["https://raw.github.com/lberwa/wii-pip"]
 
 def update():
-    get = w.curl_get("https://localhost:8000")
+    for url in urls:
+        get.append(w.curl_get(url + "/data.json"))
+    
+    
